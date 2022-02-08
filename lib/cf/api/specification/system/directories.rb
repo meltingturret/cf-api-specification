0c439e50d138590219afed66c2e10f2d58a0061e
Paul Gilligan <meltingturret@gmail.com>
2022-02-08 18:45:55 +0100

    Tempate resoruces WIP

--- lib/cf/api/specification/system/directories.rb
+++ lib/cf/api/specification/system/directories.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "erb"
+require "fileutils"
 
 module Cf
   module Api
@@ -16,25 +17,33 @@ module Cf
           resources = Dir.entries(@location) - %w[. .. _header.md admin app_features app_usage_events builds buildpacks environment_variable_groups]
           puts "Updating: #{api_output}"
           resources.each do |item|
-            path = item.gsub("_", "-")
-            file_path = File.join(api_output, path)
-            Dir.mkdir(file_path) unless File.directory?(file_path)
             operations = Dir.entries(File.join(@location, item)) - %w[. .. _header.md _object.md.erb]
-            operations.each do |resource_file|
-              template(api_output, path, resource_file) unless resource_file.nil?
+            operations.each do |file|
+              resources(api_output, item, file) unless file.nil?
             end
           end
         end
 
-        def template(api_output, path, resource_file)
-          op = resource_file.sub!(".md.erb", "")
+        def setup_path(api_output, directory, item)
+          file_name = item.gsub("_", "-")
+          root_path = File.join(api_output, directory)
+          file_path = File.join(root_path, file_name)
+          FileUtils.mkdir_p(file_path) unless File.directory?(file_path)
+          file_path
+        end
+
+        def resources(api_output, item, resource_file)
+          directory = "resources"
+          file_path = setup_path(api_output, directory, item)
+          operation = resource_file.sub!(".md.erb", "")
           begin
-            use_case = op.gsub("_", "-").sub("-", "")
-            use_case_yml = "#{File.join(path, use_case)}.yml"
+            use_case = operation.gsub("_", "-").sub("-", "")
+            use_case_yml = "#{File.join(file_path, use_case)}.yml"
             puts "Updating: #{use_case_yml}"
-            yml_data = ERB.new(File.read("#{Dir.pwd}/templates/OpenAPI-Specification.yml.erb"), trim_mode: "-").result(binding)
-            File.open(File.join(api_output, use_case_yml), "w") { |f| f.write yml_data }
-          end unless op.nil?
+            @entries = %w[get post delete]
+            yml_data = ERB.new(File.read("#{Dir.pwd}/templates/resources.yml.erb"), trim_mode: "-").result(binding)
+            File.open(use_case_yml, "w") { |f| f.write yml_data }
+          end unless operation.nil?
         end
       end
     end
